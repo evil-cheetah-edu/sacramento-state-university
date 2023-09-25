a327e54fd5e927cd25c458bac6bd99774fe46a27
Eugene M <eugene.moschyn@gmail.com>
2023-09-25 13:57:52 -0700

    add(): getters and setters

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Node.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Node.java
@@ -1,3 +1,40 @@
-public class Node
+public class Node<T>
 {
+    private T       value;
+    private Node<T> next;
+
+
+    Node()
+    {
+        value = null;
+        next  = null;
+    }
+
+    Node(T value)
+    {
+        this.value = value;
+        this.next  = null;
+    }
+
+
+    T get_value()
+    {
+        return value;
+    }
+
+    void set_value(T value)
+    {
+        this.value = value;
+    }
+
+
+    Node<T> get_next()
+    {
+        return next;
+    }
+
+    void set_next(Node<T> next)
+    {
+        this.next = next;
+    }
 }
