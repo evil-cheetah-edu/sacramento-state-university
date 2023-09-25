bb779c7308b82fc677d79e5c27f23b6d035f1b5f
Eugene M <eugene.moschyn@gmail.com>
2023-09-25 14:09:07 -0700

    add(): basic stack implementation

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Stack.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Stack.java
@@ -1,2 +1,34 @@
-public class Stack {
+public class Stack<T>
+{
+    private Node<T> head;
+
+
+    void push(T value)
+    {
+        Node<T> new_node = new Node<>();
+
+        new_node.set_value(value);
+        new_node.set_next(head);
+
+        head = new_node;
+    }
+
+    T pop()
+    {
+        if (head == null)
+            throw new RuntimeException("EmptyStackException - Stack is empty");
+
+        Node<T> temp = head;
+
+        head = head.get_next();
+        temp.set_next(null);
+
+        return temp.get_value();
+    }
+
+
+    T peek()
+    {
+        return head.get_value();
+    }
 }
