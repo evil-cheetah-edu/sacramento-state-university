83ea0e37f285327a43d4971700a7c944f3bb890b
Eugene M <eugene.moschyn@gmail.com>
2023-09-25 22:52:52 -0700

    add(): message for unknown operator

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/ExceptionMessage.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/ExceptionMessage.java
@@ -2,4 +2,6 @@ public class ExceptionMessage
 {
     public static final String EMPTY_STACK_EXCEPTION_MESSAGE = "Empty Stack Exception";
     public static final String EMPTY_QUEUE_EXCEPTION_MESSAGE = "Empty Queue Exception";
+
+    public static final String UNKNOWN_OPERATOR              = "Unknown Operator";
 }
