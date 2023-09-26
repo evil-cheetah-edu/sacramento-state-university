7fc82b37f18d9ae8764c5e4a369cb184e160a471
Eugene M <eugene.moschyn@gmail.com>
2023-09-25 22:53:49 -0700

    add(): basic i/o for debugging

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 2 - Postifix Evaluate/src/Main.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        Queue<String> queue = new Queue<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an expression:");
+        String input = scanner.next();
 
-        queue.enqueue("Hello!");
-        queue.enqueue("World!");
-        queue.enqueue("I am in Queue!");
+        System.out.println(
+                "Original Expression: " + input
+        );
+        System.out.println(
+                "Postfix Notation:    " + Postfix.to_postfix(input)
+        );
+        System.out.println(
+                "Result:              " + Integer.toString(Postfix.evaluate((input)))
+        );
 
-        System.out.println(queue.peek());
-        System.out.println();
-
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
+        scanner.close();
     }
 }
