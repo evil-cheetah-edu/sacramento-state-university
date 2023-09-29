59b06648d67699338a8c35d3652bc79a80fc14d6
Eugene M <eugene.moschyn@gmail.com>
2023-09-28 23:03:39 -0700

    add(): basic error messages

---
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 3 - Doubly Linked List/src/ExceptionMessage.java
@@ -0,0 +1,6 @@
+public class ExceptionMessage
+{
+    public static final String UNKNOWN_ORDER  = "Ordering Exception - Unknown Display Order Specified";
+    public static final String FILE_NOT_FOUND = "File Not Found - Check Specified Path";
+    public static final String FAIL_TO_DELETE = "Fail To Delete - Linked List is Empty...";
+}
