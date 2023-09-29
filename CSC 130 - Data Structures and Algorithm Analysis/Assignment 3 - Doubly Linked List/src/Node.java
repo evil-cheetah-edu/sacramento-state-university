c9d132ed11020d25767b52ef76ef4acfe73293fe
Eugene M <eugene.moschyn@gmail.com>
2023-09-28 18:37:00 -0700

    add(): double linked node functionality

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 3 - Doubly Linked List/src/Node.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 3 - Doubly Linked List/src/Node.java
@@ -1,3 +1,53 @@
-public class Node
+public class Node<T>
 {
+    private Node<T> prev;
+    private T       value;
+    private Node<T> next;
+
+
+    Node()
+    {
+        value = null;
+        prev  = null;
+        next  = null;
+    }
+
+    Node(T value)
+    {
+        this.value = value;
+        this.prev  = null;
+        this.next  = null;
+    }
+
+
+    T get_value()
+    {
+        return value;
+    }
+
+    Node<T> get_prev()
+    {
+        return prev;
+    }
+
+    Node<T> get_next()
+    {
+        return next;
+    }
+
+
+    void set_value(T value)
+    {
+        this.value = value;
+    }
+
+    void set_prev(Node<T> prev)
+    {
+        this.prev = prev;
+    }
+
+    void set_next(Node<T> next)
+    {
+        this.next = next;
+    }
 }
