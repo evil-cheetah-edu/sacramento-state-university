1435da6b2f8b7c2754d62107866b6be340bbc421
Eugene M <eugene.moschyn@gmail.com>
2023-10-15 17:12:49 -0700

    debug(): the matrix functionality

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 4 - Sort Search/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 4 - Sort Search/src/Main.java
@@ -2,6 +2,10 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        Matrix matrix1 = new Matrix(5, 4);
+        matrix1.traverse();
+
+        Matrix matrix2 = new Matrix(Constants.BACK_UP_MATRIX);
+        matrix2.traverse();
     }
 }
