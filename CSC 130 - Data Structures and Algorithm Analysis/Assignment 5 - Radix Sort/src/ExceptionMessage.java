3bfa4a455d603fb9abe4d0deb5de467309122853
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-10-25 20:32:01 -0700

    copy(): static message class for exceptions

---
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Radix Sort/src/ExceptionMessage.java
@@ -0,0 +1,7 @@
+public class ExceptionMessage
+{
+    public static final String EMPTY_STACK_EXCEPTION_MESSAGE = "Empty Stack Exception";
+    public static final String EMPTY_QUEUE_EXCEPTION_MESSAGE = "Empty Queue Exception";
+
+    public static final String UNKNOWN_OPERATOR              = "Unknown Operator";
+}
