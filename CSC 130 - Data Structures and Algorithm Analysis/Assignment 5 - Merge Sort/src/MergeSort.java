9c9dc2be7c59bd969cf8c056e257bdcea197b380
Eugene M <eugene.moschyn@gmail.com>
2023-11-07 02:33:02 -0800

    add(): intermediate solution (doesn't work)

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/MergeSort.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/MergeSort.java
@@ -1,3 +1,51 @@
+import java.util.Comparator;
+
+
 public class MergeSort
 {
+   static LinkedList<Integer> sort(LinkedList<Integer> items, Comparator<Integer> comparator)
+   {
+        if (items.size() == 0 || items.size() == 1)
+            return items;
+
+        LinkedList<Integer> left  = items.split();
+        LinkedList<Integer> right = items;
+
+        left  = sort(left,  comparator);
+        right = sort(right, comparator);
+
+        return merge(left, right, comparator);
+   }
+
+
+    private static LinkedList<Integer> merge(
+        LinkedList<Integer> left,
+        LinkedList<Integer> right,
+        Comparator<Integer> comparator
+    )
+    {
+        LinkedList<Integer> result = new LinkedList<>();
+
+        while ( !left.is_empty() && !right.is_empty() )
+        {
+            if ( left.get_head_value() > right.get_head_value() )
+            {
+                result.add_to_back(right.get_head_value());
+                right.pop_in_front();
+            }
+            else
+            {
+                result.add_to_back(left.get_head_value());
+                left.pop_in_front();
+            }
+        }
+
+        if ( left != null )
+            result.merge(left);
+
+        if ( right != null )
+            result.merge(right);
+
+        return result;
+    }
 }
