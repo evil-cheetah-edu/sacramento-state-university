97cf519f56e11e9f138787df2e04f093fac95003
Eugene M <eugene.moschyn@gmail.com>
2023-11-07 01:33:04 -0800

    add(): debug for testing purposes

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/Main.java
@@ -2,6 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add_to_back(2);
+        list.add_to_back(3);
+        list.add_to_back(10);
+        list.add_to_back(20);
+
+        LinkedList<Integer> list2 = list.split();
+
+        list.traverse();
+        System.out.println();
+        list2.traverse();
     }
 }
