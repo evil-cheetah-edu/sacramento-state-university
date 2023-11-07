a3b8c93cab22725db4012c049491a8525d7e7f20
Eugene M <eugene.moschyn@gmail.com>
2023-11-07 02:48:19 -0800

    add(): debug for ascending and descending order

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 5 - Merge Sort/src/Main.java
@@ -13,10 +13,17 @@ public class Main
         list.add_to_back(20);
         list.add_to_back(2);
 
+        System.out.println("Original Linked List:");
         list.traverse();
         System.out.println();
 
+        System.out.println("Sorted Linked List(Ascending):");
         list = MergeSort.sort(list, Comparator.naturalOrder());
+        list.traverse();
+        System.out.println();
+
+        System.out.println("Sorted Linked List(Descending):");
+        list = MergeSort.sort(list, Comparator.reverseOrder());
         list.traverse();
     }
 }
