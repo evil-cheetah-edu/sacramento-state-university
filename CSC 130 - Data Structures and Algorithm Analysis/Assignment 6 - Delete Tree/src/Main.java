3faae3ce9d8815b2e4d3b3b35b3415af5c30a521
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-11-27 20:16:04 -0800

    add(): debug logic for delete

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
@@ -20,5 +20,17 @@ public class Main
 
         System.out.println("Post Order:");
         tree.traverse(TraversalType.POST_ORDER);
+
+        System.out.println();
+        tree.delete(10);
+
+        System.out.println("Pre Order:");
+        tree.traverse(TraversalType.PRE_ORDER);
+
+        System.out.println("In Order:");
+        tree.traverse(TraversalType.IN_ORDER);
+
+        System.out.println("Post Order:");
+        tree.traverse(TraversalType.POST_ORDER);
     }
 }
