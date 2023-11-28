d81bc5616f6aa634f2297c5eddf2327d9e080196
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-11-27 18:13:24 -0800

    add(): insert method

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/BinaryTree.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/BinaryTree.java
@@ -1,3 +1,29 @@
 public class BinaryTree
 {
+    private Node<Integer> root;
+
+    BinaryTree()
+    {
+        root = null;
+    }
+
+    void insert(int value)
+    {
+        root = insert(root, new Node<Integer>(value));
+    }
+
+
+    private Node<Integer> insert(Node<Integer> current, Node<Integer> new_node)
+    {
+        if ( current == null )
+            return new_node;
+
+        if ( new_node.get_value() < current.get_value() )
+            current.set_left( new_node );
+
+        else if ( new_node.get_value() > current.get_value() )
+            current.set_right( new_node );
+
+        return current;
+    }
 }
