13e6f2bca536c8b753423d00be18fb83ee55fb8a
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-11-27 20:16:47 -0800

    add(): file not found error

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/ExceptionMessage.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/ExceptionMessage.java
@@ -1,4 +1,6 @@
 public class ExceptionMessage
 {
     static final String UNKNOWN_TRAVERSAL_TYPE = "Unknown Traversal Type...";
+
+    static final String FILE_NOT_FOUND         = "File Not Found...";
 }
