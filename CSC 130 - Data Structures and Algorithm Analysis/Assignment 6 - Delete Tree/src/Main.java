039499e65ad282428fad6cc1b67715ed109e58f4
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-11-27 18:47:11 -0800

    add(): logic for debugging

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
@@ -2,6 +2,23 @@ public class Main
 {
     public static void main(String[] args)
     {
+        BinaryTree tree = new BinaryTree();
 
+        tree.insert(10);
+        tree.insert(6);
+        tree.insert(2);
+        tree.insert(9);
+        tree.insert(15);
+        tree.insert(11);
+        tree.insert(19);
+
+        System.out.println("Pre Order:");
+        tree.traverse(TraversalType.PRE_ORDER);
+
+        System.out.println("In Order:");
+        tree.traverse(TraversalType.IN_ORDER);
+
+        System.out.println("Post Order:");
+        tree.traverse(TraversalType.POST_ORDER);
     }
 }
