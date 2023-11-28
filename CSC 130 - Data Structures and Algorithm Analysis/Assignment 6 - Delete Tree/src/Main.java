5a6c45723e2ffb619186db621312486a633fa09d
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-11-27 20:21:10 -0800

    remove(): unused logic

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 6 - Delete Tree/src/Main.java
@@ -4,25 +4,7 @@ public class Main
     {
         BinaryTree tree = new BinaryTree();
 
-        tree.insert(10);
-        tree.insert(6);
-        tree.insert(2);
-        tree.insert(9);
-        tree.insert(15);
-        tree.insert(11);
-        tree.insert(19);
-
-        System.out.println("Pre Order:");
-        tree.traverse(TraversalType.PRE_ORDER);
-
-        System.out.println("In Order:");
-        tree.traverse(TraversalType.IN_ORDER);
-
-        System.out.println("Post Order:");
-        tree.traverse(TraversalType.POST_ORDER);
-
-        System.out.println();
-        tree.delete(10);
+        tree.read_file("input.txt");
 
         System.out.println("Pre Order:");
         tree.traverse(TraversalType.PRE_ORDER);
