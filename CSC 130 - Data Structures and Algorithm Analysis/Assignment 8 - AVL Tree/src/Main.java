b8a3f88b9da1bc33907dba028b3e0a18d0a26c5c
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-12-08 22:50:26 -0800

    remove(): unused logic

--- CSC 130 - Data Structures and Algorithm Analysis/Assignment 8 - AVL Tree/src/Main.java
+++ CSC 130 - Data Structures and Algorithm Analysis/Assignment 8 - AVL Tree/src/Main.java
@@ -6,15 +6,6 @@ public class Main
 
         tree.read_file("input.txt");
 
-        System.out.println("Pre Order:");
-        tree.traverse(TraversalType.PRE_ORDER);
-
-        System.out.println("In Order:");
-        tree.traverse(TraversalType.IN_ORDER);
-
-        System.out.println("Post Order:");
-        tree.traverse(TraversalType.POST_ORDER);
-
         System.out.println("Breadth First:");
         tree.traverse(TraversalType.BREADTH_FIRST);
     }
