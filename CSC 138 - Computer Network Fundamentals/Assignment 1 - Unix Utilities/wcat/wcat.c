7250513028876f34948ddfc722a1e0e0d550726a
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-02-03 20:45:01 -0800

    add(): file opening

--- CSC 138 - Computer Network Fundamentals/Assignment 1 - Unix Utilities/wcat/wcat.c
+++ CSC 138 - Computer Network Fundamentals/Assignment 1 - Unix Utilities/wcat/wcat.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(int argc, char* argv[])
 {
-    printf("%s\n", argv[0]);
+    FILE* file = fopen(argv[1], "r");
+
+    if ( !file )
+    {
+        printf("An error occurred while opening the file\n");
+
+        exit(1);
+    }
+
+    fclose(file);
 
     return 0;
 }
