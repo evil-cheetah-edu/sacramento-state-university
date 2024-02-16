8451a353a3f5b2572b5b70fceada476e3a80fd47
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-02-15 18:06:57 -0800

    update(): implementation

--- CSC 138 - Computer Network Fundamentals/Assignment 1 - Unix Utilities/initial-utilities/wunzip/wunzip.c
+++ CSC 138 - Computer Network Fundamentals/Assignment 1 - Unix Utilities/initial-utilities/wunzip/wunzip.c
@@ -3,14 +3,7 @@
 
 
 #define FILES_ARGUMENT_INDEX (1)
-#define STRUCT_ITEMS         (2)
-
-
-struct EncodedData
-{
-    uint32_t number_of_characters;
-    char     character;          
-};
+#define READ_SUCCESS         (1)
 
 
 int main(int argc, char* argv[])
@@ -19,7 +12,7 @@ int main(int argc, char* argv[])
     if (argc == 1)
     {
         printf("wunzip: file1 [file2 ...]\n");
-        exit(1);
+        return 1;
     }
 
     /// Iterate over Arguments
@@ -30,39 +23,30 @@ int main(int argc, char* argv[])
         /// Check if File opens
         if ( !file )
         {
-            printf("wcat: cannot open file\n");
+            printf("wunzip: cannot open file\n");
+            return 1;
+        }
+
+        int character_count;
+        char character;
+
+        /// Iterate over the File
+        while( 
+            fread(&character_count, sizeof(int),  1, file) == READ_SUCCESS &&
+            fread(&character,       sizeof(char), 1, file) == READ_SUCCESS
+        )
+        {
+            for (uint32_t i = 0; i < character_count; ++i)
+                printf("%c", character);
+        }
+
+        if ( !feof(file) )
+        {
+            printf("wunzip: File is corrupt... Terminating...");
             exit(1);
         }
 
-        /// Iterate over the File
-        while( !feof(file) )
-        {
-            struct EncodedData* data = malloc(sizeof *data);
 
-            int scan_result = fscanf(
-                file,
-                "%d%c",
-                &data->number_of_characters,
-                &data->character
-            );
-
-            if ( scan_result == EOF )
-                break;
-
-            if ( scan_result != STRUCT_ITEMS )
-            {
-                printf("\n");
-                printf("An archive seems to be corrupt...\n");
-                printf("Terminating...\n");
-
-                return 1;
-            }
-
-            for (uint32_t i = 0; i < data->number_of_characters; ++i)
-                printf("%c", data->character);
-        }
-
-        printf("\n");
         fclose(file);
     }
 
