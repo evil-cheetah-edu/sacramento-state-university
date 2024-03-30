101a12c4b2a1e7cf950dbdd2e05134b87bdf48a4
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-03-29 19:05:26 -0700

    add(): message handling

--- CSC 138 - Computer Network Fundamentals/Assignment 2 - Chat Application/client/src/main.c
+++ CSC 138 - Computer Network Fundamentals/Assignment 2 - Chat Application/client/src/main.c
@@ -12,6 +12,11 @@
 #define PORT "12345"
 #define BUFFER_SIZE 1024
 
+#define PROGRAM_NAME (argv[0])
+#define HOST         (argv[1])
+#define USERNAME     (argv[2])
+
+
 /**
  * Function: initialize_connection
  * -------------------------------
@@ -57,15 +62,38 @@ void handle_server_message(int sock);
  */
 void handle_user_input(int sock, const char *username);
 
+
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 // The main function and any additional helper functions or definitions as necessary.
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     // Step 1: Validate command-line arguments.
     // TODO: Ensure there are exactly three arguments: the executable name, the server's hostname, and the username.
+    if ( argc != 3 )
+    {
+        fprintf(stderr, "Usage: %s <hostname> <username>\n", PROGRAM_NAME);
+        exit(1);
+    }
 
     // Step 2: Initialize connection to the server.
     // TODO: Call initialize_connection with the server's hostname and port number to establish a connection.
+    int socket_fd = initialize_connection(HOST, PORT);
+
+    if ( socket_fd == -1 )
+    {
+        exit(1);
+    }
 
     // Step 3: Main loop - Use select() to multiplex between stdin (user input) and the socket (incoming messages).
     // TODO: Inside the loop, do the following:
@@ -73,9 +101,42 @@ int main(int argc, char *argv[]) {
     // b. Call select() to wait for activity on either stdin or the socket.
     // c. If select indicates activity on the server socket, call handle_server_message to process it.
     // d. If select indicates activity on stdin, call handle_user_input to read the user's message and send it to the server.
+    fd_set master;
+    fd_set read_fds;
+    int fdmax;
+
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
+
+    FD_SET(STDIN_FILENO, &master);
+    FD_SET(socket_fd, &master);
+    fdmax = socket_fd;
+
+    while (1)
+    {
+        read_fds = master;
+
+        if ( select(fdmax + 1, &read_fds, NULL, NULL, NULL) == -1 )
+        {
+            fprintf(stderr, "Error while selecting Socket Descriptor...\n");
+            perror("select");
+            exit(1);
+        }
+
+        if ( FD_ISSET(STDIN_FILENO, &read_fds) )
+        {
+            handle_user_input(socket_fd, USERNAME);
+        }
+
+        if ( FD_ISSET(socket_fd, &read_fds) )
+        {
+            handle_server_message(socket_fd);
+        }
+    }
 
     // Step 4: Clean up and close the socket before exiting.
     // TODO: Properly close the socket descriptor when the program is terminating.
+    close(socket_fd);
 
     return 0; // Exit the program.
 }
@@ -84,16 +145,15 @@ int main(int argc, char *argv[]) {
 int initialize_connection(const char *server_hostname, const char *port)
 {
     int status;
-    int socket_fd;
-    struct addrinfo hints;
-    struct addrinfo *server_info;
-    struct addrinfo *p;
+    int socket_fd = 0;
+    struct addrinfo hints, *server_info, *p;
+    char s[INET6_ADDRSTRLEN];
 
     memset(&hints, 0, sizeof(hints));
 
     hints.ai_family   = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags    = AI_PASSIVE;
+    // hints.ai_flags    = AI_PASSIVE;
 
     if ( (status = getaddrinfo(server_hostname, port, &hints, &server_info)) != 0 )
     {
@@ -109,9 +169,7 @@ int initialize_connection(const char *server_hostname, const char *port)
 
     for (p = server_info; p != NULL; p = p->ai_next)
     {
-        int socket_fd =  socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-
-        if ( socket_fd == -1 )
+        if ( (socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1 )
         {
             fprintf(stderr, "Failed creating a Socket...\n");
             perror("client: socket");
@@ -121,7 +179,7 @@ int initialize_connection(const char *server_hostname, const char *port)
         if ( connect(socket_fd, p->ai_addr, p->ai_addrlen) == -1 )
         {
             fprintf(stderr, "Failed connecting to the Host...\n");
-            perror("cleint: connect");
+            perror("client: connect");
             close(socket_fd);
             continue;
         }
@@ -135,6 +193,9 @@ int initialize_connection(const char *server_hostname, const char *port)
         return -1;
     }
 
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof(s));
+    fprintf(stdout, "client: connecting to %s\n", s);
+    
     freeaddrinfo(server_info);
 
     return socket_fd;
@@ -155,5 +216,21 @@ void handle_server_message(int sock)
 
     buffer[message_length] = '\0';
     
-    fprintf(stdout, "%s\n", buffer);
+    fprintf(stdout, "%s", buffer);
+}
+
+
+void handle_user_input(int sock, const char *username)
+{
+    char message[BUFFER_SIZE];
+    char payload[BUFFER_SIZE + 50];
+
+    fgets(message, BUFFER_SIZE, stdin);
+    sprintf(payload, "%s: %s", username, message);
+
+    if ( send(sock, payload, strlen(payload), 0) == -1 )
+    {
+        fprintf(stderr, "Failed sending the data to the Server...\n");
+        perror("send");
+    }
 }
