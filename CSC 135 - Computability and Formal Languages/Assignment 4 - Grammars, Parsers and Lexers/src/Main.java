a6135eef417b2f9acbd8083ad4543a9d25549ac8
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-04-13 22:58:04 -0700

    add(): main to test recognizer

--- CSC 135 - Computability and Formal Languages/Assignment 4 - Grammars, Parsers and Lexers/src/Main.java
+++ CSC 135 - Computability and Formal Languages/Assignment 4 - Grammars, Parsers and Lexers/src/Main.java
@@ -1,7 +1,26 @@
+/**
+ * Grammar: json
+ * Grammar Path: https://github.com/antlr/grammars-v4/tree/master/json
+ * Reasoning: JSON (JavaScript Object Notation) is the most common
+ *            format used for API communication
+ * Explanation: The second input is not accepted because glossary is
+ *              not enclosed in quotes, causing it not to match any of
+ *              the acceptable types specified in
+ *              `value` (JSON.g4, Line 28).
+**/
+
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.printf("Hello and welcome!");
+        /// Iterates over the filenames passed as args
+        /// Can be initialized manually
+        for (String arg : args)
+        {
+            String status = Recognizer.is_valid(arg) ? "accepted" : "rejected";
+
+            System.out.printf("File: %s is %s\n", arg, status);
+        }
     }
 }
