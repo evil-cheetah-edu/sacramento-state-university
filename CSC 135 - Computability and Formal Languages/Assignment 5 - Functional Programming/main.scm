8c721c4283d95c38a4351e4df2cf4fe5450d3eb2
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2024-04-27 21:08:10 -0700

    add(): maxmin implementation

--- CSC 135 - Computability and Formal Languages/Assignment 5 - Functional Programming/main.scm
+++ CSC 135 - Computability and Formal Languages/Assignment 5 - Functional Programming/main.scm
@@ -7,13 +7,51 @@
     )
 )
 
+
 ; (len `<List>) Implementation
 (define (len list)
-    (letrec ((length_func (lambda (slice result)
-        (if (null? slice)
-            result
-            (length_func (cdr slice) (+ result 1))
+    ; Recursion Function Definition
+    (letrec (
+        (length_func (lambda (slice result)
+            (if (null? slice)
+                result
+                (length_func (cdr slice) (+ result 1))
+            ))
         )
-    )))
+    )
+    
+    ; Main Call
     (length_func list 0))
+)
+
+
+; (maxmin `<List>) Implementation
+(define (maxmin list)
+    (if (null? list)
+        ; Throw Exception if `list` is empty 
+        (error "Empty list has no max or min")
+        
+        ; Main Call
+        (letrec (
+            ; Function Definition
+            (maxmin_func (lambda (list current-max current-min)
+                (if (null? list)
+                    (cons current-max current-min)
+                    (
+                        let (
+                            (head (car list))
+                            (tail (cdr list))
+                        )
+                        (maxmin_func tail 
+                            (if (> head current-max) head current-max)
+                            (if (< head current-min) head current-min)
+                        )
+                    )
+                ))
+            )
+        )
+
+        ; Function Call
+        (maxmin_func (cdr list) (car list) (car list)))
+    )
 )
